58ed504908fe48806d9b1e82e04de92f850760b4
Guoson <928111134@qq.com>
2019-03-07 16:46:14 +0800

    更改AGR名

--- com/guo/fire_meeting_room.py
+++ com/guo/fire_meeting_room.py
@@ -43,7 +43,7 @@ def multi_thread():
 
     while int(time.strftime('%H', time.localtime(time.time()))) < 10:
         # while int(time.strftime('%M', time.localtime(time.time()))) < 44:
-        time.sleep(.1)
+        time.sleep(.15)
     for d in drivers:
         try:
             threading.Thread(target=lambda: d.execute_script(
